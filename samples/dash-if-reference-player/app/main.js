dc0043c16729341f2e274745b3e71258219c3c26
EmceeCiao <mchowdhury1224@gmail.com>
2025-04-19 18:06:18 -0400

    L2A Enabled

--- samples/dash-if-reference-player/app/main.js
+++ samples/dash-if-reference-player/app/main.js
@@ -560,16 +560,19 @@ app.controller("DashController", [
             streaming: { 
                 abr: { 
                     rules: {
-                        // Let's make sure BOLA is the only thing activated 
+                        // Let's make sure all default rules are deactivated
                         throughputRule: {active: false}, 
                         learnToAdaptRule: {active: false}, 
-                        bolaRule: {active: true}, 
+                        bolaRule: {active: false}, 
 
+                        //Now let's enable L2A 
+                        l2ARule: {active: true}, 
+                        
                         //Now let's make sure we keep the "safeguard" rules in place that come on by default in dash.js are on + 
                         // -> These are used in production to provide better QOE, so keep on by default when possible 
                         abandonRequestsRule: {active: true}, 
                         switchHistoryRule: {active: true}, 
-                        insufficientBufferRule: {active: true}
+                        insufficientBufferRule: {active: true} 
                     }
                 }
             } 
