ef2cb1eb8905aba5e35a8c1100739ec9f31255c2
Jie Chen <jiechenmc@gmail.com>
2025-04-27 13:37:26 -0400

    add ws close event to playback_ended callback

--- samples/dash-if-reference-player/app/main.js
+++ samples/dash-if-reference-player/app/main.js
@@ -754,25 +754,19 @@ app.controller("DashController", [
         $scope.stallCount = 0; // Count of stalls
 
         // Add logic to monitor any stalls
-        $scope.player.on(
-            dashjs.MediaPlayer.events.BUFFER_EMPTY,
-            function () {
-                console.log("[ALERT] Playback stalled");
-                $scope.stallStartTime = performance.now(); // Record the time when stalling starts
-                $scope.stallCount++; // Increment stall count
-            }
-        );
+        $scope.player.on(dashjs.MediaPlayer.events.BUFFER_EMPTY, function () {
+            console.log("[ALERT] Playback stalled");
+            $scope.stallStartTime = performance.now(); // Record the time when stalling starts
+            $scope.stallCount++; // Increment stall count
+        });
 
-        $scope.player.on(
-            dashjs.MediaPlayer.events.BUFFER_LOADED,
-            function () {
-                if ($scope.stallStartTime) {
-                    $scope.totalStallTime +=
-                        (performance.now() - $scope.stallStartTime) / 1000; // Add stall duration in seconds
-                    $scope.stallStartTime = null; // Reset stall start time
-                }
+        $scope.player.on(dashjs.MediaPlayer.events.BUFFER_LOADED, function () {
+            if ($scope.stallStartTime) {
+                $scope.totalStallTime +=
+                    (performance.now() - $scope.stallStartTime) / 1000; // Add stall duration in seconds
+                $scope.stallStartTime = null; // Reset stall start time
             }
-        );
+        });
 
         $scope.player.on(
             dashjs.MediaPlayer.events.PLAYBACK_ENDED,
@@ -2816,7 +2810,7 @@ app.controller("DashController", [
         const socket = new WebSocket("ws://localhost:6789");
 
         $scope.plotPoint = function (name, type, value, time) {
-            if (type === "video") {
+            if (type === "video" && socket.readyState === WebSocket.OPEN) {
                 // Send Data to Server
                 const dataPoint = JSON.stringify({
                     testName: document.title,
@@ -2999,7 +2993,12 @@ app.controller("DashController", [
                     $scope.plotPoint("liveLatency", type, liveLatency, time);
                     $scope.plotPoint("playbackRate", type, playbackRate, time);
                     $scope.plotPoint("stallRate", type, stallRate, time);
-                    $scope.plotPoint("stallCount", type, $scope.stallCount, time);
+                    $scope.plotPoint(
+                        "stallCount",
+                        type,
+                        $scope.stallCount,
+                        time
+                    );
 
                     if (httpMetrics) {
                         $scope.plotPoint(
